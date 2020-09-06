b3384b3ca9a726ca0d38d9e8c9dcf79ff62ecb1c
yenarue <yenarue@gmail.com>
2020-09-07 03:11:29 +0900

    add frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app" class="is-centered">
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+    <router-view />
+  </div>
+</template>
+
+<style>
+html, body {
+  margin: 0;
+  height: 100%;
+  overflow: hidden;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: #fafafa;
+  height: 100%;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<style lang="scss">
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+// Set your colors
+$primary: #8c67ef;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099FF;
+$twitter-invert: findColorInvert($twitter);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+    "white": ($white, $black),
+    "black": ($black, $white),
+    "light": ($light, $light-invert),
+    "dark": ($dark, $dark-invert),
+    "primary": ($primary, $primary-invert),
+    "info": ($info, $info-invert),
+    "success": ($success, $success-invert),
+    "warning": ($warning, $warning-invert),
+    "danger": ($danger, $danger-invert),
+    "twitter": ($twitter, $twitter-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <div style="font-size: 4rem"><strong>C.A.R.E</strong> Profile</div>
+
+    <div class="content">
+    창조자
+    추진자
+    개선자
+    실행자
+    순응자(촉진자)
+    </div>
+
+<!--    <div class="content">-->
+<!--    서로 같은 목적을 달성하기 위해<br>-->
+<!--    서로 다른 사람들과 함께 팀으로 일할 때,<br>-->
+<!--    자신만의 <b>자연스러운 사고</b>와 <b>선호하는 행동이 더해진다면</b><br>-->
+<!--    <b>다함께 시너지를 발휘하게 될 수 있어요!</b><br>-->
+<!--    <br>-->
+<!--    <b>개인이 사용하는 다양한 접근 방식들을 이해</b>하고<br>-->
+<!--    서로의 <b>역할을 인식</b>함으로서<br>-->
+<!--    <b>보다 높은 팀 성과를 도출할 수 있을거에요!</b>-->
+<!--    </div>-->
+
+
+    <b-button class="is-primary" rounded>👉🏻 나는 어떤 유형일까? 테스트하러 가기!</b-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
